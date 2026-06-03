bd94fb9ad22fc4e98bb96fe3acc6ebb66e7a2ffc
Younes Bachouche <younesbachouche1@gmail.com>
2026-06-03 03:56:35 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -45,12 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => env('MYSQL_ADDON_URI', env('DATABASE_URL')),
+            'host' => env('MYSQL_ADDON_HOST', env('DB_HOST', '127.0.0.1')),
+            'port' => env('MYSQL_ADDON_PORT', env('DB_PORT', '3306')),
+            'database' => env('MYSQL_ADDON_DB', env('DB_DATABASE', 'forge')),
+            'username' => env('MYSQL_ADDON_USER', env('DB_USERNAME', 'forge')),
+            'password' => env('MYSQL_ADDON_PASSWORD', env('DB_PASSWORD', '')),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
